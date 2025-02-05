17c57dab57770720f41cba50cb07c511f1a032b3
La Thanh Hanh <lthanhh03@gmail.com>
2025-02-05 01:48:49 +0000

    ok

--- AutoTestSystem/BLL/bd.cs
+++ AutoTestSystem/BLL/bd.cs
@@ -156,7 +156,7 @@ namespace AutoTestSystem.BLL
             }
             else
             {
-                loggerInfo(txt);
+                logger.Info(txt);
 
             }
             SaveLog(txt);
